1713d5dc7689cff5cc6ca802105e57654f7fa464
vladboisa <vladboisa@gmail.com>
2024-04-28 20:26:18 +0300

    feat: add check on changing form before update

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -1,40 +1,42 @@
-import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Donut } from '../../models/donut.model';
 
 @Component({
   selector: 'app-donut-form',
   templateUrl: './donut-form.component.html',
-  styleUrls: ['./donut-form.component.scss']
+  styleUrls: ['./donut-form.component.scss'],
 })
 export class DonutFormComponent {
+  isFormChanged = false;
   @Input() donut!: Donut;
   @Output() createForm = new EventEmitter<Donut>();
   @Output() updateForm = new EventEmitter<Donut>();
-icons :string[] = [
-  'caramel',
-  'chocolate',
-  'default',
-  'glazed',
-  'white'
-]
+  @ViewChild('donut-form') form!: NgForm;
+  icons: string[] = ['caramel', 'chocolate', 'default', 'glazed', 'white'];
 
-constructor () {
+  constructor() { }
 
-}
-
-handleCreate(form: NgForm) {
-  if (form.valid) {
-    this.createForm.emit(form.value)
-  } else {
-    form.form.markAllAsTouched();
+  handleCreate(form: NgForm) {
+    if (form.valid) {
+      this.createForm.emit(form.value);
+    } else {
+      form.form.markAllAsTouched();
+    }
   }
-}
-handleUpdate(form:NgForm) {
-  if (form.valid) {
-    this.updateForm.emit({id: this.donut.id, ...form.value})
-  } else {
-    form.form.markAllAsTouched();
+  handleUpdate(form: NgForm) {
+    if (form.valid && this.isFormChanged) {
+      this.updateForm.emit({ id: this.donut.id, ...form.value });
+      this.isFormChanged = false;
+    } else {
+      form.form.markAllAsTouched();
+    }
   }
-}
 }
