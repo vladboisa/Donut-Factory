556e7dd7fbd0ea04502ad1bbecbb0017dc707f6d
vladboisa <vladboisa@gmail.com>
2024-05-04 19:05:02 +0300

    refactor: move db from service to db.json file

--- src/app/admin/services/donut.service.ts
+++ src/app/admin/services/donut.service.ts
@@ -5,41 +5,7 @@ import { Donut } from '../models/donut.model';
   providedIn: 'root'
 })
 export class DonutService {
-  private donuts : Donut[] = [{
-    id: 'yHgsaf',
-    name: 'Chocolate',
-    icon: 'chocolate',
-    price:119,
-    promo: 'limited',
-    description: 'Chocolate'
-  },
-  {
-    id: 'yHgasfasgsaf',
-    name: 'Glazed',
-    icon: 'glazed',
-    price:179,
-    promo: 'new',
-    description: 'Glazed as fuck'
-  },
-  {
-    id: 'yagsgasgHgsaf',
-    name: 'Caramel',
-    icon:'caramel',
-    price:129,
-    description: 'Cara cara mel'
-    },
-  {id: 'kZ8422',
-  name: 'Default donut',
-  icon:'default',
-  price:89,
-  description: 'Tasty default'
-  },
-  {id: 'fSap21',
-  name: 'White',
-  icon:'white',
-  price:109,
-  description: 'Sooooo fully white'
-  }];
+  private donuts : Donut[] = [];
   constructor() { }
   readAll () {
     return this.donuts;
