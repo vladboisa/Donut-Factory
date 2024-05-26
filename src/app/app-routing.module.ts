ccb56f0390c0b47106c106cb1089fe95d61aa730
vladboisa <vladboisa@gmail.com>
2024-05-26 18:41:37 +0300

    feat: add admin routing option

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { DonutListComponent } from './admin/containers/donut-list/donut-list.component';
 import { DonutSingleComponent } from './admin/containers/donut-single/donut-single.component';
 
-const routes: Routes = [{path: '', component: DonutListComponent},
+const routes: Routes = [{path: 'admin',
+children: [{path: 'donuts', component: DonutListComponent},
 {path:'donut', component: DonutSingleComponent},
-{path: '**', redirectTo: ''}];
+{path: '', redirectTo: 'donuts', pathMatch: 'full'}
+]},
+{path: '', redirectTo: 'admin', pathMatch: 'full'},
+{path: '**', redirectTo: 'admin'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
