adfb27b0d83cfd7f6297f8a90a72f740ec6973be
vladboisa <vladboisa@gmail.com>
2024-05-26 13:56:11 +0300

    feat: add routes & router outlen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DonutListComponent } from './admin/containers/donut-list/donut-list.component';
+import { DonutSingleComponent } from './admin/containers/donut-single/donut-single.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: DonutListComponent},
+{path:'donut', component: DonutSingleComponent},
+{path: '**', redirectTo: ''}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
