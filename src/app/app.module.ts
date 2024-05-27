076ae3a453e69563a5150e4e8c1994ec18717064
vladboisa <vladboisa@gmail.com>
2024-05-27 19:14:29 +0300

    refactor: clear unnecassary adminModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +11,6 @@ import { AdminModule } from './admin/admin.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
