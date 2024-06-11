1b3a869ed9215cb1a52db5d0557dfb93b549d659
vladboisa <vladboisa@gmail.com>
2024-06-11 20:19:15 +0300

    feat: take id by RouteParam&refactor logic

--- src/app/admin/containers/donut-single/donut-single.component.ts
+++ src/app/admin/containers/donut-single/donut-single.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Donut } from '../../models/donut.model';
 import { DonutService } from '../../services/donut.service';
+import { ActivatedRoute } from '@angular/router';
+import { map, take } from 'rxjs';
 
 @Component({
   selector: 'app-donut-single',
@@ -9,18 +11,21 @@ import { DonutService } from '../../services/donut.service';
 })
 export class DonutSingleComponent {
   donut!: Donut;
-  constructor(private donutService: DonutService) {}
+  constructor(
+    private donutService: DonutService,
+    private activatedRoute: ActivatedRoute
+  ) {}
   ngOnInit() {
-    let id:Donut["id"] = '80b3';
-    this.donutService.createdDonutId$.subscribe({
-      next: (createdId) => this.donut.id = createdId
-    });
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
     this.donutService.readOneById(id).subscribe((donut) => {
       this.donut = donut;
     });
-  }
-  ngOnDestroy() {
-    this.donutService.createdDonutId$.unsubscribe();
+    this.donutService.createdDonutId$
+      .pipe(
+        take(1),
+        map((createdId) => (this.donut.id = createdId))
+      )
+      .subscribe();
   }
 
   onCreate(donut: Donut) {
@@ -28,14 +33,14 @@ export class DonutSingleComponent {
   }
   onUpdate(donut: Donut) {
     this.donutService.update(donut).subscribe({
-      next:undefined,
-      error: (err) => console.log(`Error from Update : ${console.dir(err)}`)
+      next: undefined,
+      error: (err) => console.log(`Error from Update : ${console.dir(err)}`),
     });
   }
   onDelete(donut: Donut) {
     this.donutService.delete(donut).subscribe({
-      next:undefined,
-      error: (err) => console.log(`Error from Update : ${console.dir(err)}`)
+      next: undefined,
+      error: (err) => console.log(`Error from Update : ${console.dir(err)}`),
     });
   }
 }
