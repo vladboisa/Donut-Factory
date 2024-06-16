7dd354a438776a5ebf5cf9a57d3112799bf1d193
vladboisa <vladboisa@gmail.com>
2024-06-16 19:28:03 +0300

    feat: add data for rouring

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -10,8 +10,8 @@ import { DonutCardComponent } from './components/donut-card/donut-card.component
 import { DonutFormComponent } from './components/donut-form/donut-form.component';
 
 const routes:Routes = [{path: 'donuts', component: DonutListComponent},
-{path:'donuts/new', component: DonutSingleComponent},
-{path: 'donuts/:id', component: DonutSingleComponent},
+{path:'donuts/new', component: DonutSingleComponent, data: {isEditable:false}},
+{path: 'donuts/:id', component: DonutSingleComponent, data: {isEditable:true}},
 {path: '', redirectTo: 'donuts', pathMatch: 'full'}
 ];
 
