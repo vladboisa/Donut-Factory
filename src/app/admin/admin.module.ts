1f3b6272d2733beec03614e08dfbd25350c0ecfa
vladboisa <vladboisa@gmail.com>
2024-07-07 19:32:30 +0300

    feat: add lazy load components

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -9,10 +9,31 @@ import { DonutSingleComponent } from './containers/donut-single/donut-single.com
 import { DonutCardComponent } from './components/donut-card/donut-card.component';
 import { DonutFormComponent } from './components/donut-form/donut-form.component';
 
-const routes:Routes = [{path: 'donuts', component: DonutListComponent},
-{path:'donuts/new', component: DonutSingleComponent, data: {isEditable:false}},
-{path: 'donuts/:id', component: DonutSingleComponent, data: {isEditable:true}},
-{path: '', redirectTo: 'donuts', pathMatch: 'full'}
+const routes: Routes = [
+  {
+    path: 'donuts',
+    loadComponent: () =>
+      import('../admin/containers/donut-list/donut-list.component').then(
+        (x) => x.DonutListComponent
+      ),
+  },
+  {
+    path: 'donuts/new',
+    loadComponent: () =>
+      import('../admin/containers/donut-single/donut-single.component').then(
+        (x) => x.DonutSingleComponent
+      ),
+    data: { isEditable: false },
+  },
+  {
+    path: 'donuts/:id',
+    loadComponent: () =>
+      import('../admin/containers/donut-single/donut-single.component').then(
+        (x) => x.DonutSingleComponent
+      ),
+    data: { isEditable: true },
+  },
+  { path: '', redirectTo: 'donuts', pathMatch: 'full' },
 ];
 
 @NgModule({
@@ -20,11 +41,9 @@ const routes:Routes = [{path: 'donuts', component: DonutListComponent},
     DonutListComponent,
     DonutCardComponent,
     DonutSingleComponent,
-    DonutFormComponent
+    DonutFormComponent,
   ],
-  imports: [
-    CommonModule,FormsModule, RouterModule.forChild(routes)
-  ],
-  exports: [DonutListComponent, DonutSingleComponent]
+  imports: [CommonModule, FormsModule, RouterModule.forChild(routes)],
+  exports: [DonutListComponent, DonutSingleComponent],
 })
-export class AdminModule { }
+export class AdminModule {}
