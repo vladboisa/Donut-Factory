0cba7211628baba49b45b3f41175665e7e8ec98c
vladboisa <vladboisa@gmail.com>
2024-08-06 18:02:29 +0100

    feat: add layout of register

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['../login/login.component.scss']
 })
 export class RegisterComponent {
 
