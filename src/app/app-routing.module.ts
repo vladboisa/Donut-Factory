96d06e27ee760817331ac0a46d15af177b2d1ab6
vladboisa <vladboisa@gmail.com>
2024-08-09 17:20:22 +0100

    feat: add loading module to the router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 const routes: Routes = [{path: 'admin',
 loadChildren: ()=> import('./admin/admin.module').then(x=>x.AdminModule)},
 {path: '', redirectTo: 'admin', pathMatch: 'full'},
+{path: '', loadChildren: () => import ('./auth/auth.module').then(x=>x.AuthModule)},
 {path: '**', redirectTo: 'admin'}];
 
 @NgModule({
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+//component's
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+
+const routes = [{path: 'login', component: LoginComponent},
+{path: 'register', component: RegisterComponent}];
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
   imports: [
-    CommonModule, FormsModule
+    CommonModule, FormsModule, RouterModule.forChild(routes)
   ],
-  exports : [LoginComponent]
+  exports : []
 })
 export class AuthModule { }
