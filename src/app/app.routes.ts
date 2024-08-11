cf82a72b10f5857daee5435bc84e6c95512075e4
vladboisa <vladboisa@gmail.com>
2024-08-11 14:05:34 +0100

    refactor: change admin module to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { Routes } from '@angular/router';
+import { DonutService } from './admin/services/donut.service';
 
 export const appRoutes: Routes = [{path: 'admin',
-loadChildren: ()=> import('./admin/admin.module').then(x=>x.AdminModule)},
+loadChildren: ()=> import('./admin/admin.routes').then(x=>x.adminRoutes),
+providers: [importProvidersFrom(HttpClientModule), DonutService]},
 {path: '', redirectTo: 'admin', pathMatch: 'full'},
 {path: '**', redirectTo: 'admin'}];
 
