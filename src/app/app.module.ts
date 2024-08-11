4a20ef321e73b1fa2d298a240eda95786c4ec59a
vladboisa <vladboisa@gmail.com>
2024-08-11 14:09:14 +0100

    refactor: remove declarations ngMod to standalone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     AuthModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
