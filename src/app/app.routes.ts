7b805e142831f5d5b06dc2fc0193157659f3c7c3
vladboisa <vladboisa@gmail.com>
2024-08-15 14:32:20 +0100

    refactor: formatt to Prettier& delete NgModule App

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { Routes } from '@angular/router';
 import { DonutService } from './admin/services/donut.service';
 
-export const appRoutes: Routes = [{path: 'admin',
-loadChildren: ()=> import('./admin/admin.routes').then(x=>x.adminRoutes),
-providers: [importProvidersFrom(HttpClientModule), DonutService]},
-{path: '', redirectTo: 'admin', pathMatch: 'full'},
-{path: '**', redirectTo: 'admin'}];
-
+export const appRoutes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.routes').then((x) => x.adminRoutes),
+    providers: [importProvidersFrom(HttpClientModule), DonutService],
+  },
+  { path: '', redirectTo: 'admin', pathMatch: 'full' },
+  { path: '**', redirectTo: 'admin' },
+];
