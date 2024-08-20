8a07bada58673ae00b8e82016436933813cedec9
vladboisa <vladboisa@gmail.com>
2024-08-20 19:59:07 +0100

    feat: add provider for not duplicating FormModule

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,14 @@ import { importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { Routes } from '@angular/router';
 import { DonutService } from './admin/services/donut.service';
+import { FormsModule } from '@angular/forms';
 
 export const appRoutes: Routes = [
   {
     path: 'admin',
     loadChildren: () =>
       import('./admin/admin.routes').then((x) => x.adminRoutes),
-    providers: [importProvidersFrom(HttpClientModule), DonutService],
+    providers: [importProvidersFrom(HttpClientModule), DonutService, importProvidersFrom(FormsModule)],
   },
   { path: '', redirectTo: 'admin', pathMatch: 'full' },
   {path: '', loadChildren: () => import ('./auth/auth.routes').then(x=>x.authRoutes) },
