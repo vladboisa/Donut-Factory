79851023e3c30d00bad2321b54257f09f5b0ac20
vladboisa <vladboisa@gmail.com>
2024-09-08 17:07:09 +0100

    feat: provide interceptor into bootstrapApp

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
-
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoaderInterceptor } from './app/shared/interceptors/loader.interceptor';
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(RouterModule.forRoot(appRoutes))]
-})
-  .catch(err => console.error(err));
+  providers: [
+    importProvidersFrom(RouterModule.forRoot(appRoutes)),
+    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true },
+  ],
+}).catch((err) => console.error(err));
