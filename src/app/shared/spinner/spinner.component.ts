7adbf1deaf493322c735c9bfe05c039e808a13b0
vladboisa <vladboisa@gmail.com>
2024-09-08 17:04:19 +0100

    feat: provide spinnerService into component

--- src/app/shared/spinner/spinner.component.ts
+++ src/app/shared/spinner/spinner.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SpinnerService } from '../services/spinner.service';
 
 @Component({
   selector: 'app-spinner',
@@ -9,5 +10,7 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./spinner.component.scss']
 })
 export class SpinnerComponent {
+constructor (public spinnerService: SpinnerService) {
 
+}
 }
