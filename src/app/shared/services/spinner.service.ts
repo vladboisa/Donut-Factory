be9ed528441a2a74c336c89d55bd0cd9ee5cc68c
vladboisa <vladboisa@gmail.com>
2024-09-08 17:06:17 +0100

    feat: add logic for Spinner

--- src/app/shared/services/spinner.service.ts
+++ src/app/shared/services/spinner.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpinnerService {
-
+  private isLoadingSubject = new BehaviorSubject<boolean>(true);
+  isLoading$ = this.isLoadingSubject.asObservable();
   constructor() { }
+  showSpinner() {
+    this.isLoadingSubject.next(true)
+  }
+  hideSpinner() {
+    this.isLoadingSubject.next(false)
+  }
 }
