ef477353d70d01fef1e3788cf32acb35ca5d00d3
vladboisa <vladboisa@gmail.com>
2024-09-09 19:53:44 +0100

    feat: add spinner into AppComponentr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SpinnerComponent],
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
