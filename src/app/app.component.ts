25b49adb983cd39579459253e8088dcaabf6e444
vladboisa <vladboisa@gmail.com>
2024-09-13 19:46:11 +0100

    feat: add spinnerService to the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { SpinnerComponent } from './shared/spinner/spinner.component';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { SpinnerService } from './shared/services/spinner.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [RouterModule, SpinnerComponent],
+  imports: [RouterModule, SpinnerComponent, CommonModule],
+  providers: [SpinnerComponent],
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  isLoading: SpinnerService;
+  constructor (private activatedRoute: ActivatedRoute) {
+    console.log(this.isLoading);
+  }
 }
