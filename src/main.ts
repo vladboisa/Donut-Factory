0807cb42d41e07bfc74ba4acb6f4b801e0a738fb
vladboisa <vladboisa@gmail.com>
2024-09-19 18:40:15 +0100

    feat: add provideHttp with interceptors

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { LoaderInterceptor } from './app/shared/interceptors/loader.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(RouterModule.forRoot(appRoutes)),
+    provideHttpClient(withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true },
   ],
 }).catch((err) => console.error(err));
